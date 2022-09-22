54b7b3b0ccb22ee09e5c8b5990e075a599024929
kostadubovskiy <kostadubovskiy@gmail.com>
2022-09-22 09:25:36 -0400

    krewes

--- 04_choose/krewes.py
+++ 04_choose/krewes.py
@@ -1,17 +1,20 @@
 """
 Craig Chen, Aaron Gershkovich, Kosta Dubovskiy
 SoftDev
+04-krewes
 -Dictionaries and Random- 
 2022-09-22
-time spent:
+time spent: 30 minutes
 DISCO:
     * The key in the dictionary can be anything, and it's useful for categorization of many things
     * Need to import random with lowercase
     * Need to randint needs two arguments
     * randint is inclusive on both ends
+    * random.choice(sequence) returns a random element
 QCC:
     * Is there a way to generate a random item from an input list, with a built in random method
 OPS SUMMARY:
+    * We made a function, that given a dictionary with key:array pairs returns a random element of a random array
 """
 
 krewes = {2:["devo1", "devo2", "devo3", "devo4", "devo5"], 7:["devo1", "devo2", "devo3", "devo4", "devo5"], 8:["devo1", "devo2", "devo3", "devo4", "devo5"]}
@@ -19,8 +22,7 @@ krewes = {2:["devo1", "devo2", "devo3", "devo4", "devo5"], 7:["devo1", "devo2", 
 import random
 
 def get_devo(dictionary):
-    randKeyInd = random.randint(0, 2)
-    randKey = list(dictionary.keys())[randKeyInd]
+    randKey = random.choice(list(dictionary.keys()))
     randDevo = random.randint(0, len(dictionary[randKey]) - 1)
     return dictionary[randKey][randDevo]
 
