78d28955ec54533851660ecc960afcf5157836ec
Aaron Gershkovich <agershkovich30@stuy.edu>
2022-10-03 21:30:22 -0400

    did QCC and OPS

--- 07_teardown/app.py
+++ 07_teardown/app.py
@@ -1,5 +1,11 @@
-
-# your heading here
+"""
+Craig Chen, Aaron Gershkovich, Kosta Dubovskiy
+SoftDev
+07-app
+-Apps and Flask-
+2022-10-03
+time spent: 20 minutes
+"""
 
 from flask import Flask
 
@@ -18,6 +24,11 @@ DISCO:
 * Our program does not work for unanticipated reasons
 * We host the app locally
 QCC:
+-We are getting a circular error that we don't know how to fix
+-We can't install or unistall the app to try again
+-We seem to be the only ones with this issue
+
+Questions:
 0. Java init functions are similar, the __[]__ usually symbolizes something default
 1. It means root directory often
 2. To the app root directory, since we routed everything there
@@ -26,8 +37,10 @@ QCC:
 5. We've seen similar stuff in Flutter and React/JS
 ...
 INVESTIGATIVE APPROACH:
-<Your concise summary of how
- you and your team set about
- "illuminating the cave of ignorance" here...>
- * 
+We initially read throught the given code, identifying that it was using an app
+to create a local website on the computer, then we looked through the question
+to see if it could shed any light on the content, which helped us understand the
+subworkings of the program. Then we looked at a successful run of the program
+to see if it did what we thought it would.
+ *
 '''
